497f7273720131766a63a97402f3890384c32543
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-05-04 05:47:30 -0700

    project created and condfiguered

---
+++ flutter_portfolio/lib/core/app_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  MaterialApp build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
