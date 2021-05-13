073b5548ca502d0955b9ea9534b644202c4ab5e3
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-05-12 17:33:07 -0700

    improving about me part #01

--- flutter_portfolio/lib/core/components/widgets/body/body_section.dart
+++ flutter_portfolio/lib/core/components/widgets/body/body_section.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter_portfolio/core/services/open_link_service.dart';
 import '../../app_sizedbox.dart';
 import '../../../components/app/app_text_style.dart';
 // import '../body/widgets/custom_body_title_widget.dart';
 
 class BodySection extends StatelessWidget {
+  dynamic get _hereText {
+    return TextButton(
+      onPressed: () => OpenLinkService.openLink(
+          'https://github.com/MarceloJSCesar/markdown-portfolio'),
+      child: Text('Here'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -70,6 +79,7 @@ class BodySection extends StatelessWidget {
                     text: [
                       'About me :',
                       'Nice to meet you',
+                      'About me :',
                     ],
                     speed: Duration(milliseconds: 200),
                     pause: Duration(seconds: 3),
@@ -82,7 +92,7 @@ class BodySection extends StatelessWidget {
                   Container(
                     padding: const EdgeInsets.only(left: 20, top: 15),
                     child: Text(
-                      'Hi there! It\'s a pleasure to have you here, seeing my portfolio ',
+                      'Hi there ! I\'m Marcelo Cesar, 17 years, from Cape Verde. \n ${'I\'m'.padLeft(5)} that person passsionate about computer world, studying as much as possible to be able fix the real world\'s problems and to realize my dreams, desires, etc \n ${'I\'m'.padLeft(5)} currently learning, exploring Dart, Flutter to get build any apps according with its difficult but my real is cyber security, right now studying computer network(OSI Model).',
                       style: TextStyle(color: Colors.grey, fontSize: 15),
                     ),
                   ),
